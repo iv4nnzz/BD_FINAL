56828e55aa579326e41b6cf4f51e895cf733d1de
Rollyzx <ivanagudelo.5050@gmail.com>
2026-05-22 20:12:42 -0500

    actualizar ssl a true

--- src/Datos/Conexion.java
+++ src/Datos/Conexion.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 
 public class Conexion {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/gestion_servicios?useSSL=false&useUnicode=true&characterEncoding=UTF-8";
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_servicios?useSSL=true&useUnicode=true&characterEncoding=UTF-8";
     private static final String USUARIO  = "root";
     private static final String CONTRASENA = "root"; 
 
